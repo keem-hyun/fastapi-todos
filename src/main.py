21bef7a96cc4e9f9520e81ff1268541cc8c0cde7
keem-hyun <hw813@naver.com>
2024-09-15 19:57:10 +0900

    HTTP Response 처리

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from database.connection import get_db
 from database.orm import ToDo
 from database.repository import get_todos
+from schema.response import ListToDoResponse, ToDoSchema
 
 app = FastAPI()
 
@@ -39,11 +40,15 @@ todo_data = {
 def get_todos_handler(
         order: str | None = None,
         session: Session = Depends(get_db)
-):
+) -> ListToDoResponse:
     todos: List[ToDo] = get_todos(session=session)
     if order and order == "DESC":
-        return todos[::-1]
-    return todos
+        return ListToDoResponse(
+            todos=[ToDoSchema.from_orm(todo) for todo in todos[::-1]]
+        )
+    return ListToDoResponse(
+        todos=[ToDoSchema.from_orm(todo) for todo in todos]
+    )
 
 
 @app.get("/todos/{todo_id}", status_code=200)
